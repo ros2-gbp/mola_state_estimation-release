b219fd93fd4579d4cc9670a16e087527827794af
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-05-25 17:41:42 +0200

    Rebase from 'debian/kilted/mola_state_estimation_simple'

--- src/Parameters.cpp
+++ src/Parameters.cpp
@@ -35,6 +35,12 @@ void Parameters::loadFrom(const mrpt::containers::yaml& cfg)
 
     MCP_LOAD_OPT(cfg, sigma_random_walk_acceleration_linear);
     MCP_LOAD_OPT(cfg, sigma_random_walk_acceleration_angular);
+
+    MCP_LOAD_OPT(cfg, sigma_relative_pose_linear);
+    MCP_LOAD_OPT(cfg, sigma_relative_pose_angular);
+
+    MCP_LOAD_OPT(cfg, sigma_imu_angular_velocity);
+
     MCP_LOAD_OPT(cfg, enforce_planar_motion);
 
     MCP_LOAD_OPT(cfg, do_process_imu_labels);

729e902254c499ee6ef3fe96e3eee728fcc67940
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-09-07 23:41:01 +0200

    Rebase from 'release/kilted/mola_state_estimation_smoother'

--- include/mola_state_estimation_smoother/FactorTricycleKinematics.h
+++ include/mola_state_estimation_smoother/FactorTricycleKinematics.h
@@ -11,36 +11,30 @@
  */
 #pragma once
 
-#include <mola_kernel/factors/FactorBase.h>
+#include <mola_state_estimation_smoother/id.h>
 #include <mrpt/core/exceptions.h>
 
 namespace mola
 {
 /** Abstract representation of a constant-velocity tricycle kinematic motion
  * model factor between two key frames.
- *
- * \ingroup mola_kernel_factors_grp
  */
-class FactorTricycleKinematics : public FactorBase
+class FactorTricycleKinematics
 {
-    DEFINE_SERIALIZABLE(FactorTricycleKinematics, mola)
-
    public:
     FactorTricycleKinematics() = default;
 
     /** Creates relative pose constraint of KF `to` as seem from `from`. */
-    FactorTricycleKinematics(id_t kf_from, id_t kf_to, double deltaTime)
-        : from_kf_(kf_from), to_kf_(kf_to), deltaTime_(deltaTime)
+    FactorTricycleKinematics(
+        id_t kf_from, id_t kf_to, double delta_time)  // NOLINT
+        : from_kf(kf_from), to_kf(kf_to), deltaTime(delta_time)
     {
     }
 
-    id_t from_kf_ = INVALID_ID, to_kf_ = INVALID_ID;
+    id_t from_kf = INVALID_ID, to_kf = INVALID_ID;
 
     /** Elapsed time between "from_kf" and "to_kf" [seconds] */
-    double deltaTime_ = .0;
-
-    std::size_t edge_count() const override { return 2; }
-    mola::id_t  edge_indices(const std::size_t i) const override;
+    double deltaTime = .0;
 };
 
 }  // namespace mola

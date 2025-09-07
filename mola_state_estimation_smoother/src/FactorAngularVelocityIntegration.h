5ac869a79a8a2633715125b1db63e6764209adf6
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-09-07 23:43:21 +0200

    Rebase from 'upstream'

--- mola_state_estimation_smoother/src/FactorAngularVelocityIntegration.h
+++ mola_state_estimation_smoother/src/FactorAngularVelocityIntegration.h
@@ -1,22 +1,17 @@
-/* -------------------------------------------------------------------------
- *   A Modular Optimization framework for Localization and mApping  (MOLA)
- *
- * Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria
- * Licensed under the GNU GPL v3 for non-commercial applications.
- *
- * This file is part of MOLA.
- * MOLA is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- * MOLA is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * MOLA. If not, see <https://www.gnu.org/licenses/>.
- * ------------------------------------------------------------------------- */
+/*               _
+ _ __ ___   ___ | | __ _
+| '_ ` _ \ / _ \| |/ _` | Modular Optimization framework for
+| | | | | | (_) | | (_| | Localization and mApping (MOLA)
+|_| |_| |_|\___/|_|\__,_| https://github.com/MOLAorg/mola
+
+ Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria,
+                         and individual contributors.
+ SPDX-License-Identifier: GPL-3.0
+ See LICENSE for full license information.
+ Closed-source licenses available upon request, for this odometry package
+ alone or in combination with the complete SLAM system.
+*/
+
 /**
  * @file   FactorAngularVelocityIntegration.h
  * @brief  GTSAM factor
@@ -31,6 +26,7 @@
 #include <gtsam/nonlinear/ExpressionFactor.h>
 #include <gtsam/nonlinear/expressions.h>
 #include <gtsam/slam/expressions.h>
+#include <mola_state_estimation_smoother/gtsam_detect_version.h>
 
 namespace mola::state_estimation_smoother
 {
@@ -43,25 +39,28 @@ namespace mola::state_estimation_smoother
  *   gtsam::between( gtsam::compose(Ri, expmap_), Rj ) = Rot_Identity
  *
  */
-class FactorAngularVelocityIntegration
-    : public gtsam::ExpressionFactorN<
-          gtsam::Rot3 /*return type*/, gtsam::Rot3 /* Ri */,
-          gtsam::Point3 /* bWi */, gtsam::Rot3 /* Rj */
-          >
+class FactorAngularVelocityIntegration : public gtsam::ExpressionFactorN<
+                                             gtsam::Rot3 /*return type*/, gtsam::Rot3 /* Ri */,
+                                             gtsam::Point3 /* bWi */, gtsam::Rot3 /* Rj */
+                                             >
 {
    private:
     using This = FactorAngularVelocityIntegration;
     using Base = gtsam::ExpressionFactorN<
-        gtsam::Rot3 /*return type*/, gtsam::Rot3 /* Ri */,
-        gtsam::Point3 /* bWi */, gtsam::Rot3 /* Rj */
+        gtsam::Rot3 /*return type*/, gtsam::Rot3 /* Ri */, gtsam::Point3 /* bWi */,
+        gtsam::Rot3 /* Rj */
         >;
 
     double dt_ = .0;
 
    public:
-    /// default constructor
     FactorAngularVelocityIntegration()           = default;
     ~FactorAngularVelocityIntegration() override = default;
+
+    FactorAngularVelocityIntegration(const FactorAngularVelocityIntegration&)            = default;
+    FactorAngularVelocityIntegration& operator=(const FactorAngularVelocityIntegration&) = default;
+    FactorAngularVelocityIntegration(FactorAngularVelocityIntegration&&)                 = default;
+    FactorAngularVelocityIntegration& operator=(FactorAngularVelocityIntegration&&)      = default;
 
     FactorAngularVelocityIntegration(
         gtsam::Key kRi, gtsam::Key kbWi, gtsam::Key kRj, const double dt,
@@ -74,8 +73,12 @@ class FactorAngularVelocityIntegration
     /// @return a deep copy of this factor
     gtsam::NonlinearFactor::shared_ptr clone() const override
     {
-        return boost::static_pointer_cast<gtsam::NonlinearFactor>(
+#if GTSAM_USES_BOOST
+        return boost::static_pointer_cast<This>(
             gtsam::NonlinearFactor::shared_ptr(new This(*this)));
+#else
+        return std::static_pointer_cast<gtsam::NonlinearFactor>(std::make_shared<This>(*this));
+#endif
     }
 
     // Return measurement expression
@@ -95,21 +98,19 @@ class FactorAngularVelocityIntegration
 
     /** print */
     void print(
-        const std::string& s, const gtsam::KeyFormatter& keyFormatter =
-                                  gtsam::DefaultKeyFormatter) const override
+        const std::string&         s,
+        const gtsam::KeyFormatter& keyFormatter = gtsam::DefaultKeyFormatter) const override
     {
-        std::cout << s << "FactorAngularVelocityIntegration("
-                  << keyFormatter(Factor::keys_[0]) << ","
-                  << keyFormatter(Factor::keys_[1]) << ","
-                  << keyFormatter(Factor::keys_[2]) << ")\n";
+        std::cout << s << "FactorAngularVelocityIntegration(" << keyFormatter(Factor::keys_[0])
+                  << "," << keyFormatter(Factor::keys_[1]) << "," << keyFormatter(Factor::keys_[2])
+                  << ")\n";
         gtsam::traits<gtsam::Rot3>::Print(measured_, "  measured: ");
         gtsam::traits<double>::Print(dt_, "  dt: ");
         this->noiseModel_->print("  noise model: ");
     }
 
     /** equals */
-    bool equals(const gtsam::NonlinearFactor& expected, double tol = 1e-9)
-        const override
+    bool equals(const gtsam::NonlinearFactor& expected, double tol = 1e-9) const override
     {
         const This* e = dynamic_cast<const This*>(&expected);
         return e != nullptr && Base::equals(*e, tol) &&
@@ -117,6 +118,7 @@ class FactorAngularVelocityIntegration
     }
 
    private:
+#if GTSAM_USES_BOOST
     /** Serialization function */
     friend class boost::serialization::access;
     template <class ARCHIVE>
@@ -128,9 +130,9 @@ class FactorAngularVelocityIntegration
         ar& BOOST_SERIALIZATION_NVP(measured_);
         ar& BOOST_SERIALIZATION_NVP(dt_);
         ar& boost::serialization::make_nvp(
-            "FactorAngularVelocityIntegration",
-            boost::serialization::base_object<Base>(*this));
+            "FactorAngularVelocityIntegration", boost::serialization::base_object<Base>(*this));
     }
+#endif
 };
 
 }  // namespace mola::state_estimation_smoother

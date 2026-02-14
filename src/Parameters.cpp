cbcb7f250e38efec900b9fb738c1b939f165da31
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2026-02-14 11:07:53 +0100

    Rebase from 'rpm/jazzy/mola_state_estimation_simple'

--- src/Parameters.cpp
+++ src/Parameters.cpp
@@ -38,22 +38,9 @@ void Parameters::loadFrom(const mrpt::containers::yaml& cfg)
 
     MCP_LOAD_OPT(cfg, enforce_planar_motion);
 
-    {
-        std::string do_process_imu_labels;
-        MCP_LOAD_OPT(cfg, do_process_imu_labels);
-        do_process_imu_labels_re = do_process_imu_labels;
-    }
-
-    {
-        std::string do_process_odometry_labels;
-        MCP_LOAD_OPT(cfg, do_process_odometry_labels);
-        do_process_odometry_labels_re = do_process_odometry_labels;
-    }
-    {
-        std::string do_process_gnss_labels;
-        MCP_LOAD_OPT(cfg, do_process_gnss_labels);
-        do_process_gnss_labels_re = do_process_gnss_labels;
-    }
+    MCP_LOAD_OPT(cfg, do_process_imu_labels_re);
+    MCP_LOAD_OPT(cfg, do_process_odometry_labels_re);
+    MCP_LOAD_OPT(cfg, do_process_gnss_labels_re);
 
     if (cfg.has("initial_twist"))
     {

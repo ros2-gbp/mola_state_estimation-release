ff8451be202a86ace87fb3eb659dcdc62bb07adf
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2026-04-29 11:56:40 +0200

    Revert "Trimmed the branch to only the mola_state_estimation sub directory"
    This reverts commit d815d8b3bda44057ba32cf66e29ee04e93ffad48.

---
+++ mola_gtsam_factors/src/FactorAngularVelocityIntegration.cpp
@@ -0,0 +1,29 @@
+/*               _
+ _ __ ___   ___ | | __ _
+| '_ ` _ \ / _ \| |/ _` | Modular Optimization framework for
+| | | | | | (_) | | (_| | Localization and mApping (MOLA)
+|_| |_| |_|\___/|_|\__,_| https://github.com/MOLAorg/mola
+
+ Copyright (C) 2018-2026 Jose Luis Blanco, University of Almeria,
+                         and individual contributors.
+ SPDX-License-Identifier: GPL-3.0
+ See LICENSE for full license information.
+ Closed-source licenses available upon request, for this odometry package
+ alone or in combination with the complete SLAM system.
+*/
+
+/**
+ * @file   FactorAngularVelocityIntegration.cpp
+ * @brief  GTSAM factor
+ * @author Jose Luis Blanco Claraco
+ * @date   Jun 13, 2024
+ */
+
+#include <mola_gtsam_factors/FactorAngularVelocityIntegration.h>
+
+namespace mola::factors
+{
+
+FactorAngularVelocityIntegration::FactorAngularVelocityIntegration() = default;
+
+}
